1e532522afca56ed193c8af47213e1433f661d06
Hack Reactor Students <sjsnider@gmail.com>
2014-04-21 17:49:22 -0700

    dancer is blinking

--- src/dancer.js
+++ src/dancer.js
@@ -1,33 +1,39 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
-
-  var dancer = {};
-
+var Dancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
+  this._top = top;
+  this._left = left;
+  this._timeBetweenSteps = timeBetweenSteps;
+  this.step();
 
+  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within the body
+  this.setPosition(this._top, this._left);
+};
 
-  dancer.step = function(){
+  Dancer.prototype.step = function(){
+    var bind = function(fn, context){
+      return function(){
+        fn.call(context);
+      };
+    };
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
+    var that = this;
+    // setTimeout.call(that, that.step, that._timeBetweenSteps);
+    setTimeout(bind(that.step, that), that._timeBetweenSteps);
   };
-  dancer.step();
 
-  dancer.setPosition = function(top, left){
+// unsure about arguments
+  Dancer.prototype.setPosition = function(_top, _left){
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
     var styleSettings = {
-      top: top,
-      left: left
+      top: _top,
+      left: _left
     };
-    dancer.$node.css(styleSettings);
+    this.$node.css(styleSettings);
   };
 
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
-
-  return dancer;
-};
